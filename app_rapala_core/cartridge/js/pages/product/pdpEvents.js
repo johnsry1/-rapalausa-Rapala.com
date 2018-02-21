1cdbb64a4b0c3060da009b50f8e5d044f3959baf
sboyle <sboyle@lyonscg.com>
2018-02-21 18:34:01 -0500

    RPS-208 iOS cannot read e.charCode nor e.keyCode so I needed to add e.which to pull in the correct value.

--- app_rapala_core/cartridge/js/pages/product/pdpEvents.js
+++ app_rapala_core/cartridge/js/pages/product/pdpEvents.js
@@ -2203,8 +2203,8 @@ var pdpEvents = {
             }
         });
         $('#Quantity').keyup(function(e) {
-            var key = e.charCode || e.keyCode || 0;
-            if ($.isNumeric($('#Quantity').val()) && (key == 13 || key == 0)) {
+            var key = e.charCode || e.keyCode || e.which || 0;
+            if ($.isNumeric($('#Quantity').val()) && (key == 13)) {
                 $('#add-to-cart').trigger('click');
             } else if (!$.isNumeric($('#Quantity').val())) {
                 $('.addtocartbutton:last').prop('disabled', true);
