b118c83e9f8d331882b34c3f244c38dc64c19108
sboyle <sboyle@lyonscg.com>
2018-02-21 16:56:47 -0500

    RPS_208 - removing the backspace keycode check because  if someone did enter 15 in the quantity field and hit the backspace key, the value will now be 1 and it will submit.

--- app_rapala_core/cartridge/js/pages/product/pdpEvents.js
+++ app_rapala_core/cartridge/js/pages/product/pdpEvents.js
@@ -2204,7 +2204,7 @@ var pdpEvents = {
         });
         $('#Quantity').keyup(function(e) {
             var key = e.charCode || e.keyCode || 0;
-            if ($.isNumeric($('#Quantity').val()) && (key == 13 || key == 8 || key == 0)) {
+            if ($.isNumeric($('#Quantity').val()) && (key == 13 || key == 0)) {
                 $('#add-to-cart').trigger('click');
             } else if (!$.isNumeric($('#Quantity').val())) {
                 $('.addtocartbutton:last').prop('disabled', true);
