2d3929a83fe630835fb9c34891120ab4525b8bff
Soum <sghosh@Ghosh-MacBook-Pro.local>
2018-07-03 13:34:38 -0400

    RPS-268 -- corrected logic

--- app_rapala_core/cartridge/js/app.js
+++ app_rapala_core/cartridge/js/app.js
@@ -554,8 +554,7 @@ function initializeDom() {
     }
     // load js specific styles//
     util.limitCharacters();
-    // user session timeout//
-    util.userTimeout(dialog);
+    window.dialog = dialog;
 }
 
 var pages = {
