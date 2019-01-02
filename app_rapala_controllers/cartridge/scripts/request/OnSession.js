ef20f1e6d81045637a38c4fe3070eae80da8cdf6
Mike Kowalchuk <mkowalchuk@lyonscg.com>
2019-01-02 19:56:27 +0000

    Revert "RPS-306 - fixing redirect error when revisiting qa.rapala.com in a new window (same session) (pull request #647)"
    This reverts pull request #647.
    
    > The session variable tracking the preferred region is called “interstitialSiteId” not “interstitialSite”. We were setting the wrong variable in OnSession.js, which was causing issues when a new page or new window is opened.
    > 
    > Also making a change to use the cookie value in OnRequest.js to determine whether we need to make a redirect. This value will always exist if there should be a redirect so there’s no need to check the session variable here. We are still using the session variable in Home-ShowInLocale\(\) to determine whether we should redirect to the Shop By Brand page \(US\) or the homepage \(EU\)

--- app_rapala_controllers/cartridge/scripts/request/OnSession.js
+++ app_rapala_controllers/cartridge/scripts/request/OnSession.js
@@ -87,7 +87,7 @@ function getPreferredRegion() {
 exports.onSession = function () {
     session.custom.device = getDeviceType();
     session.custom.showCountryPopup = showCountryPopup();
-    session.custom.interstitialSiteId = getPreferredRegion();
+    session.custom.interstitialSite = getPreferredRegion();
     var InterstitialHelper = require('*/cartridge/scripts/util/InterstitialHelper');
     
     if (dw.system.Site.current.getCustomPreferenceValue('GeoIPRedirectType').value === 'session' && !session.custom.showCountryPopup) {
